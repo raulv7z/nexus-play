9137a83d8b15ec39de398c0a4dd3630ec61f47ee
raulv7z <raulzks0@gmail.com>
2024-04-26 23:11:41 +0200

    Add complementary visuals like svgs for the scheme-color toggler and enable transitions for both elements, svgs and toggler

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,36 +8,51 @@ import 'datatables.net';
 // Inits
 ////////////////////////////////////
 window.$ = window.jQuery = $;
-
 window.Alpine = Alpine;
 Alpine.start();
 
+// Vars
+////////////////////////////////////
+const html = $('html');
+const themeToggle = $('#theme-toggle');
+const themeIndicator = $('#toggle-dot');
+
 // Functions
 ////////////////////////////////////
+function startApp() {
+    initializeTheme();
+    attachEventHandlers();
+}
 
-//
+function initializeTheme() {
+    const theme = localStorage.getItem('theme') || 'light';
+    setTheme({theme: theme});
+}
 
-// Execs
+function setTheme({theme}) {
+    if (theme === 'dark') {
+        html.addClass('dark');
+        themeToggle.prop('checked', true);
+        themeIndicator.css('transform', 'translateX(1rem)');
+    } else {
+        html.removeClass('dark');
+        themeToggle.prop('checked', false);
+        themeIndicator.css('transform', 'translateX(0)');
+    }
+    localStorage.setItem('theme', theme);
+}
+
+function attachEventHandlers() {
+    themeToggle.on('change', function() {
+        setTheme({theme: this.checked ? 'dark' : 'light'});
+    });
+}
+
+// Main execution
 ////////////////////////////////////
 
 $(function() {
-    const themeToggle = $('#theme-toggle');
-    const html = $('html');
 
-    // Inicializar el estado del tema desde localStorage
-    if (localStorage.getItem('theme') === 'dark') {
-        html.addClass('dark');
-        themeToggle.prop('checked', true);
-    }
+    startApp();
 
-    // Evento de cambio para el toggle
-    themeToggle.on('change', function() {
-        if (this.checked) {
-            html.addClass('dark');
-            localStorage.setItem('theme', 'dark');
-        } else {
-            html.removeClass('dark');
-            localStorage.setItem('theme', 'light');
-        }
-    });
 });
