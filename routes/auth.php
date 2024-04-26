d6503ce8dc46ee362b8c4ac997383a121f80bf9c
raulv7z <raulzks0@gmail.com>
2024-04-26 15:17:27 +0200

    First commit

--- routes/auth.php
+++ routes/auth.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+// Routes for non-authenticated users
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
@@ -35,6 +36,7 @@ Route::middleware('guest')->group(function () {
                 ->name('password.store');
 });
 
+// Routes for authenticated users
 Route::middleware('auth')->group(function () {
     Route::get('verify-email', EmailVerificationPromptController::class)
                 ->name('verification.notice');
