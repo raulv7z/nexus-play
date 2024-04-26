5ad960a71a647cfce417dcfc969fcd40760ad09e
raulv7z <raulzks0@gmail.com>
2024-04-26 16:28:40 +0200

    Add js libs like JQuery and Datatables and en-es language support

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,31 @@
+// Imports
+////////////////////////////////////
+import $ from 'jquery';
 import './bootstrap';
-
 import Alpine from 'alpinejs';
 
-window.Alpine = Alpine;
+// Requires
+////////////////////////////////////
+require('datatables.net');
+require('datatables.net-dt/css/jquery.dataTables.css');
 
+// Inits
+////////////////////////////////////
+window.$ = window.jQuery = $;
+
+window.Alpine = Alpine;
 Alpine.start();
+
+// Functions
+////////////////////////////////////
+
+//
+
+// Execs
+////////////////////////////////////
+
+$(function() {
+
+    console.log('jquery is ready');
+
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                                         href="{{ route('login') }}"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
-                                        Log in
+                                        {{ __('Log In') }}
                                     </a>
 
                                     @if (Route::has('register'))
@@ -46,7 +46,8 @@
                                             href="{{ route('register') }}"
                                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                         >
-                                            Register
+                                            <!-- {{ __('This sould be an example') }} -->
+                                            {{ __('Register') }}
                                         </a>
                                     @endif
                                 @endauth
