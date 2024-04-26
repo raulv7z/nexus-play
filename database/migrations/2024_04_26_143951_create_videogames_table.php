7d6a5b448bc890825ff7c242e265908ccffbab1a
raulv7z <raulzks0@gmail.com>
2024-04-26 17:05:44 +0200

    Add nexus-play migrations and translate it to eng for following Laravel nomenclatures

---
+++ database/migrations/2024_04_26_143951_create_videogames_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videogames', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('name', 60);
+            $table->string('description', 120);
+            $table->string('front_page', 60)->default('portada-defecto.png');
+            $table->string('developer', 30);
+            $table->string('genre', 60);
+            $table->float('base_price');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videogames');
+    }
+};
---
+++ database/migrations/2024_04_26_145517_create_editions_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('editions', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('platform_id')->constrained();
+            $table->foreignId('videogame_id')->constrained();
+            $table->float('amount');
+            $table->unsignedInteger('stock')->default(100);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('editions');
+    }
+};
