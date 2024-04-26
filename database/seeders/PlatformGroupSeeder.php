7c1308daf7a17958af65426770a4caf1eb3bd93b
raulv7z <raulzks0@gmail.com>
2024-04-26 18:11:05 +0200

    Fill PlatformGroup seeder and integrate it on views from the Composer

---
+++ database/seeders/PlatformGroupSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\PlatformGroup;
+
+class PlatformGroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+        PlatformGroup::create([
+            'name' => 'Sobremesa',
+        ]);
+        
+        PlatformGroup::create([
+            'name' => 'Play Station',
+        ]);
+
+        PlatformGroup::create([
+            'name' => 'X Box',
+        ]);
+
+        PlatformGroup::create([
+            'name' => 'Nintendo',
+        ]);
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,11 +16,19 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        Another links
+
+                <!-- Load platform links -->
+                @foreach($platformGroups as $group)
+                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    
+                    <!-- //todo change links to real routes on web.php, same with :active -->
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('COMPLETE_THIS')">
+                        {{ $group->name }}
                     </x-nav-link>
-                </div>
+                    </div>
+                @endforeach
+
+                <ul>
             </div>
 
             <!-- Settings Dropdown -->
