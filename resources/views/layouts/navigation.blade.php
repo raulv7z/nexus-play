34f8f8066aa029440e98303f234d5ba4f0ffcbf7
raulv7z <raulzks0@gmail.com>
2024-04-27 23:48:07 +0200

    Add cart to navbar althought responsive nav still broken since svgs were added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,6 +3,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
+
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
@@ -35,7 +36,7 @@
             <div class="hidden sm:flex sm:items-center sm:ms-6">
 
                 <!-- Dark/Light Mode Toggler -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex mr-5">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex mr-2">
                     
                     <input id="theme-toggle" type="checkbox" class="hidden">
                         
@@ -124,6 +125,22 @@
                         </form>
                     </x-slot>
                 </x-dropdown>
+            
+                <!-- Cart link -->
+                <a href="#" class="mr-2">
+                    <svg class="dark:hidden" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+                    
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#4a4a4a" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                
+                    </svg>
+                    
+                    <svg class="hidden dark:block" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+                        
+                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6.29977 5H21L19 12H7.37671M20 16H8L6 3H3M9 20C9 20.5523 8.55228 21 8 21C7.44772 21 7 20.5523 7 20C7 19.4477 7.44772 19 8 19C8.55228 19 9 19.4477 9 20ZM20 20C20 20.5523 19.5523 21 19 21C18.4477 21 18 20.5523 18 20C18 19.4477 18.4477 19 19 19C19.5523 19 20 19.4477 20 20Z" stroke="#9fa4c6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                    
+                    </svg>
+                </a>
+            
             </div>
 
             <!-- Hamburger -->
