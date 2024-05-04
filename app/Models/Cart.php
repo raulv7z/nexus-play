78b333dc6496c460135b11246eb6f7e7cfee55b8
raulv7z <raulzks0@gmail.com>
2024-05-04 15:43:29 +0200

    Add relations to Cart model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'iva', 'base_amount', 'full_amount', 'status', 'purchased_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function entries()
+    {
+        return $this->hasMany(CartEntry::class);
+    }
 }
