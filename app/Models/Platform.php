ee6a0d05816f1ad22e50fd702a127d26e9e3a5ef
raulv7z <raulzks0@gmail.com>
2024-05-04 15:42:28 +0200

    Add relations to Platform model

--- app/Models/Platform.php
+++ app/Models/Platform.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Platform extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'plus', 'platform_group_id'];
+
+    public function group()
+    {
+        return $this->belongsTo(PlatformGroup::class, 'platform_group_id');
+    }
+
+    public function editions()
+    {
+        return $this->hasMany(Edition::class);
+    }
 }
