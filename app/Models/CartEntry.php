c80ff9cf038f1542c75c997cd1d0a560289e353a
raulv7z <raulzks0@gmail.com>
2024-05-04 15:44:42 +0200

    Add relations to CartEntry model

--- app/Models/CartEntry.php
+++ app/Models/CartEntry.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class CartEntry extends Model
 {
     use HasFactory;
+    
+    protected $table = 'cart_entries';
+    protected $fillable = ['cart_id', 'edition_id', 'quantity'];
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function edition()
+    {
+        return $this->belongsTo(Edition::class);
+    }
 }
