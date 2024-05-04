e708505a2b4c02ebca589a5f9f2a6fe6fba30f55
raulv7z <raulzks0@gmail.com>
2024-05-04 16:57:10 +0200

    Add functionality on PlatformGroupController > show for take in count ajax requests just in a single method

--- app/Http/Controllers/PlatformGroupController.php
+++ app/Http/Controllers/PlatformGroupController.php
@@ -8,9 +8,15 @@ use Illuminate\Http\Request;
 class PlatformGroupController extends Controller
 {
     //
-    public function show($id)
+    public function show(Request $request, $id)
     {
         $platformGroup = PlatformGroup::findOrFail($id);
+    
+        if ($request->ajax()) {
+            return response()->json($platformGroup);
+        }
+    
         return view('platform-groups.show', compact('platformGroup'));
     }
+
 }
