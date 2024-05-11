b135fc87306a3ec8cf896f4bb1fb2b08a1eeaa8c
raulv7z <raulzks0@gmail.com>
2024-05-11 22:08:45 +0200

    create form request for store user profiles

---
+++ app/Http/Requests/ProfileStoreRequest.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true; // Asegúrate de ajustar la autorización según la lógica de tu aplicación.
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8'] // Asegúrate de validar la contraseña adecuadamente.
+        ];
+    }
+}
