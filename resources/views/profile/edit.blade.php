f83a0824ce7dee40f8f0fe2d4724a185bb1d18ba
raulv7z <raulzks0@gmail.com>
2024-05-11 14:52:22 +0200

    fix double div conflict on the view for edit profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -6,26 +6,20 @@
     </x-slot>
 
     <x-interface.info-block>
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                @include('profile.partials.update-profile-information-form')
-            </div>
+        <div class="max-w-xl">
+            @include('profile.partials.update-profile-information-form')
         </div>
     </x-interface.info-block>
 
     <x-interface.info-block>
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                @include('profile.partials.update-password-form')
-            </div>
+        <div class="max-w-xl">
+            @include('profile.partials.update-password-form')
         </div>
     </x-interface.info-block>
 
     <x-interface.info-block>
-        <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-            <div class="max-w-xl">
-                @include('profile.partials.delete-user-form')
-            </div>
+        <div class="max-w-xl">
+            @include('profile.partials.delete-user-form')
         </div>
     </x-interface.info-block>
 
