3b44a246685bbfebb151b6734963d26685fcbccc
raul <raulzks0@gmail.com>
2024-05-21 13:37:57 +0200

    create reviews migration

---
+++ database/migrations/2024_05_21_113511_create_reviews_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('edition_id')->constrained()->onDelete('cascade');
+            $table->decimal('rating', 10, 2)->default(0);
+            $table->string('comment', 120);
+            $table->boolean('verified')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
