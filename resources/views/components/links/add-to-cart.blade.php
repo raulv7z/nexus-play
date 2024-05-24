ada6c693d19c0daefddbff9390fb6ecebe24b201
raul <raulzks0@gmail.com>
2024-05-24 09:53:08 +0200

    unify styles for buttons

--- resources/views/components/links/add-to-cart.blade.php
+++ resources/views/components/links/add-to-cart.blade.php
@@ -2,9 +2,9 @@
 
 <a href="{{ $link }}" class="block w-full sm:w-auto">
     <button
-        class="w-full sm:w-auto bg-pink-950 text-pink-400 border border-pink-400 border-b-4 font-medium overflow-hidden relative px-2 py-1 sm:px-4 sm:py-2 rounded-full hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group text-xs sm:text-base">
+        class="w-full sm:w-auto bg-pink-950 text-pink-400 border border-pink-400 border-b-4 font-medium overflow-hidden relative px-2 py-1 sm:px-4 sm:py-2 rounded-lg hover:brightness-150 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group text-xs sm:text-base">
         <span
-            class="bg-pink-400 shadow-pink-400 absolute -top-[150%] left-0 inline-flex w-40 sm:w-80 h-[3px] sm:h-[5px] rounded-full opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
+            class="bg-pink-400 shadow-pink-400 absolute -top-[150%] left-0 inline-flex w-40 sm:w-80 h-[3px] sm:h-[5px] rounded-lg opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
         {{ __('Add to cart') }}
     </button>
 </a>
