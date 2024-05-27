29f1ec9eb8e940ed8aa873b794c0def085725d0a
raul <raulzks0@gmail.com>
2024-05-27 18:00:43 +0200

    show editions for each platform group grouping by platforms

--- app/Http/Controllers/User/PlatformGroupController.php
+++ app/Http/Controllers/User/PlatformGroupController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\PlatformGroup;
 use App\Http\Requests\StorePlatformGroupRequest;
 use App\Http\Requests\UpdatePlatformGroupRequest;
+use App\Models\Edition;
 use Illuminate\Http\Request;
 
 class PlatformGroupController extends Controller
@@ -14,6 +15,15 @@ class PlatformGroupController extends Controller
     public function show(Request $request, $id)
     {
         $platformGroup = PlatformGroup::findOrFail($id);
-        return view('content.platform-groups.show', compact('platformGroup'));
+
+        // Obtener las ediciones del platformGroup solicitado
+        $editions = Edition::whereHas('platform', function ($query) use ($platformGroup) {
+            $query->where('platform_group_id', $platformGroup->id);
+        })->get();
+
+        // Agrupar las ediciones por cada Platform del platformGroup
+        $editionsByPlatform = $editions->groupBy('platform_id');
+
+        return view('content.platform-groups.show', compact('platformGroup', 'editionsByPlatform'));
     }
 }
--- resources/views/content/platform-groups/show.blade.php
+++ resources/views/content/platform-groups/show.blade.php
@@ -7,8 +7,34 @@
         </x-interface.breadcrumbs>
     </x-slot>
 
+    <x-interface.hidden-block>
+
+        <x-interface.title :title="$platformGroup->name . ' Editions'">
+        </x-interface.title>
+
+    </x-interface.hidden-block>
+
+    {{-- ! All editions (change this) --}}
     <x-interface.info-block>
-        {{ __("Hello!") }}
+
+        @foreach ($editionsByPlatform as $platformId => $editions)
+            {{-- Título de la plataforma --}}
+            <div class="flex justify-center text-center bg-gray-300 dark:bg-gray-600 rounded-lg p-5 my-5">
+                <p>
+                    {{ \App\Models\Platform::find($platformId)->name }}
+                </p>
+            </div>
+
+            <x-sections.game-section>
+                {{-- Mostrar las ediciones de la plataforma --}}
+                @foreach ($editions as $edition)
+                    <x-cards.game-card :edition="$edition">
+                    </x-cards.game-card>
+                @endforeach
+            </x-sections.game-section>
+        @endforeach
+
     </x-interface.info-block>
+
 
 </x-layouts.app>
