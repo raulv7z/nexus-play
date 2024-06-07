cc204858edf7734d8a7afc081a4fcb2b8fe4036b
raul <raulzks0@gmail.com>
2024-06-07 12:39:56 +0200

    customize verify-email messages for a more descriptive information, fix responsive conflicts on their view and include translations

--- resources/views/partials/breeze/authentication-container.blade.php
+++ resources/views/partials/breeze/authentication-container.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <div
-        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+        class="w-full sm:max-w-lg mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
 
         @yield('form-content')
 
--- resources/views/partials/breeze/verify-email.blade.php
+++ resources/views/partials/breeze/verify-email.blade.php
@@ -2,20 +2,28 @@
 
 @section('form-content')
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __('Wait a moment!') }}
+    </div>
+
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __('Before continue, could you verify your email address by clicking on the link we just emailed to you?') }}
+    </div>
+
+    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+        {{ __('If you didn\'t receive the email, we will gladly send you another.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
         <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+            {{ __('A new verification link has been sent to ') . auth()->user()->email . '.' }}
         </div>
     @endif
 
-    <div class="mt-4 flex items-center justify-between">
+    <div class="mt-4 flex flex-col gap-y-3 sm:gap-y-0 sm:flex-row sm:items-center sm:justify-between">
         <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
-            <div>
+            <div class="flex justify-center">
                 <x-presets.primary-button>
                     {{ __('Resend Verification Email') }}
                 </x-presets.primary-button>
@@ -25,10 +33,12 @@
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit"
-                class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
-            </button>
+            <div class="flex justify-center">
+                <button type="submit"
+                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                    {{ __('Log Out') }}
+                </button>
+            </div>
         </form>
     </div>
 @endsection
