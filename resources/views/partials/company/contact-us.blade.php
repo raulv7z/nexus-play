7c5c54370ddd5ae594563f9b806ce5e33f631c85
raulv7z <raulzks0@gmail.com>
2024-06-08 08:50:42 +0200

    enhance message when sending message for contact

--- resources/views/partials/company/contact-us.blade.php
+++ resources/views/partials/company/contact-us.blade.php
@@ -5,13 +5,13 @@
         {{ __('Here you can write a message explaining the reasons why you need to contact us.') }}
     </div>
 
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Our technical service will contact you via email as quickly as possible.') }}
-    </div>
-
     @if (session('status') == 'contact-message-sent')
         <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
             {{ __('Your message has been sent.') }}
+        </div>
+
+        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
+            {{ __('Our technical service will contact you via email as quickly as possible.') }}
         </div>
     @endif
 
@@ -21,7 +21,8 @@
         <div>
             <x-presets.input-label for="name" :value="__('Name')" />
 
-            <x-presets.text-input id="name" class="block mt-1 w-full" type="text" name="name" placeholder="{{__('Your name...')}}" required/>
+            <x-presets.text-input id="name" class="block mt-1 w-full" type="text" name="name"
+                placeholder="{{ __('Your name...') }}" required />
 
             <x-presets.input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
@@ -29,7 +30,8 @@
         <div class="mt-4">
             <x-presets.input-label for="email" :value="__('Email')" />
 
-            <x-presets.text-input id="email" class="block mt-1 w-full" type="text" name="email" placeholder="{{__('Your e-mail address...')}}" required/>
+            <x-presets.text-input id="email" class="block mt-1 w-full" type="text" name="email"
+                placeholder="{{ __('Your e-mail address...') }}" required />
 
             <x-presets.input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
@@ -38,7 +40,8 @@
         <div class="mt-4">
             <x-presets.input-label for="message" :value="__('Message')" />
 
-            <x-presets.textarea-input id="message" class="block mt-1 w-full" type="text" name="message" placeholder="{{__('Write here the reasons why you contact us...')}}" required/>
+            <x-presets.textarea-input id="message" class="block mt-1 w-full" type="text" name="message"
+                placeholder="{{ __('Write here the reasons why you contact us...') }}" required />
 
             <x-presets.input-error :messages="$errors->get('message')" class="mt-2" />
         </div>
